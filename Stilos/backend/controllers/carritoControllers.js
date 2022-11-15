cd9128054dd22a988082dfdce4fbee406c0731d8
AramisMenciaPC <aramismencia@gmail.com>
2022-11-15 01:31:06 -0300

    Backend de carrito funcionando a medias

--- Stilos/backend/controllers/carritoControllers.js
+++ Stilos/backend/controllers/carritoControllers.js
@@ -3,19 +3,36 @@ import { tbProd } from "../models/modelAdminProd.js";
 
 export const CarrAllProd = async (req, res) => {
     try {
-        const CarritoID = await tbCarrito.findAll({ where: { fk_usuario : req.params.id } });
-        const CarrDetalle = await tbCarrDetalle.findAll({where: {fk_id_carrito : CarritoID.id}})
+        const Carrito = await tbCarrito.findOne({ where: { fk_usuario : req.params.id } });
+        const CarrDetalle = await tbCarrDetalle.findAll({where: {fk_id_carrito : Carrito.id}})
+        res.json(CarrDetalle)
       } catch (error) {
         res.json({ msg: error.message });
       }
 }
 
 export const CarrAddProd = async (req, res) => {
-    try {
-        const CarritoID = await res.json(tbCarrito.findAll ({where : { fk_usuario : req.params.id}}))
-        const CarrDetalle = await res.json(tbCarrDetalle.findAll({where: {fk_id_carrito : CarritoID.id}}))
-        const prod = await res.json(tbProd.findAll({where: {id : req.body.idProd}}))
-    } catch (error) {
-        res.json({msg: error.message})
+    const prodQuery = await tbProd.findOne({where: {id : req.body.idProd}})
+    const Prod = JSON.parse(JSON.stringify(prodQuery))
+    const CarrQuery = await tbCarrito.findOne ({where : { fk_usuario : req.params.id }})
+    const Carrito = JSON.parse(JSON.stringify(CarrQuery))
+    console.log(JSON.stringify(Carrito)+"   "+JSON.stringify(Prod)+"   "+"Aqui llegue 1")
+    if (!CarrQuery) {
+        try {
+            const CrearCarro = await tbCarrito.create({ fk_usuario : req.params.id })
+            const CarroQuery = await tbCarrito.findOne({where : { fk_usuario : req.params.id}})
+            const Carro = JSON.parse(JSON.stringify(CarroQuery))
+            const crearDetail = await tbCarrDetalle.create({fk_id_carrito: Carro.id, fk_producto_id: Prod.id, cantidad: req.body.cantidad, estado_compra: "En carro", fecha_compra: null, precio_unitario: Prod.precio_unitario})
+            res.json({msg: "Guardado con exito"})
+        } catch (error) {
+            res.json({msg: error.message + "1"})
+        }
+    } else {
+        try {
+            const crearDetail = await tbCarrDetalle.create({fk_id_carrito: Carrito.id, fk_producto_id: Prod.id, cantidad: req.body.cantidad, estado_compra: "En carro", fecha_compra: null, precio_unitario: Prod.precio_unitario})
+            res.json({msg: "Guardado con exito"})
+        } catch (error) {
+            res.json({msg: error.message + "2"})
+        }
     }
 }
--- Stilos/backend/routes/rutaCarrito.js
+++ Stilos/backend/routes/rutaCarrito.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import { CarrAllProd,CarrAddProd } from "../controllers/carritoControllers.js"
 
 const rutas = express.Router();
+
+rutas.post('/allProds/:id',CarrAllProd)
+
+rutas.post('/newCarrProd/:id', CarrAddProd)
 
 export default rutas;
